eaebe094190d505b82799c93d99ab1b336555f50
Franklin Sarpong Boampong <frankboamps@fb.com>
2019-06-27 10:30:14 -0700

    Initial Commit

---
+++ tumblr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tumblr
+//
+//  Created by frankboamps on 6/27/19.
+//  Copyright © 2019 frankboamps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tumblr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tumblr
+//
+//  Created by frankboamps on 6/27/19.
+//  Copyright © 2019 frankboamps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
